747a58ec0f677b7a2cc8c63771f0d80ceab8eb82
Adrian Horvath <horviskoadrianko@gmail.com>
2022-01-18 16:49:13 +0100

    polaris finance names

--- src/App.tsx
+++ src/App.tsx
@@ -87,7 +87,7 @@ const Providers: React.FC = ({ children }) => {
             walletconnect: { rpcUrl: config.defaultProvider },
             walletlink: {
               url: config.defaultProvider,
-              appName: 'Tomb Finance',
+              appName: 'Polaris Finance',
               appLogoUrl: 'https://github.com/tombfinance/tombfinance-assets/blob/master/logo_tomb_NoBG.png',
             },
           }}
