0d64c12fdd37b20ad544c700d8a394e89b2dbdd2
horviskoadrianko <horviskoadrianko@gmail.com>
2022-02-06 20:52:10 +0100

    fix logo remove dc and tg

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -66,10 +66,10 @@ const Footer = () => {
             >
               <img alt="github" src={GithubImage} className={classes.img} />
             </a>
-            <a href="https://t.me/polarisfinance" rel="noopener noreferrer" target="_blank" className={classes.link}>
+            <a href="" rel="noopener noreferrer" target="_blank" className={classes.link}>
               <img alt="telegram" src={TelegramImage} className={classes.img} />
             </a>
-            <a href="https://discord.gg/ugaEVmm6xa" rel="noopener noreferrer" target="_blank" className={classes.link}>
+            <a href="" rel="noopener noreferrer" target="_blank" className={classes.link}>
               <img alt="discord" src={DiscordImage} className={classes.img} />
             </a>
           </Grid>
