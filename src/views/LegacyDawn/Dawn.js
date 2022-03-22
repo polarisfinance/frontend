52d15ce93c507f7858c742ad1aab13f497a52c81
horviskoadrianko <horviskoadrianko@gmail.com>
2022-03-22 18:42:56 +0100

    add legacy dawn

---
+++ src/views/LegacyDawn/Dawn.js
@@ -0,0 +1,149 @@
+import React from 'react';
+import { useWallet } from 'use-wallet';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import Bank from '../Bank';
+
+import { Box, Container, Typography, Grid } from '@material-ui/core';
+
+import { Alert } from '@material-ui/lab';
+
+import UnlockWallet from '../../components/UnlockWallet';
+import Page from '../../components/Page';
+import CemeteryCard from './DawnCard';
+import CemeteryImage from '../../assets/img/cemetery.png';
+import { createGlobalStyle } from 'styled-components';
+
+import useBanks from '../../hooks/useBanks';
+import HomeImage from '../../assets/img/home.png';
+
+const BackgroundImage = createGlobalStyle`
+  body {
+    background: url(${HomeImage}) no-repeat !important;
+    background-size: cover !important;
+    background-position: center center !important;
+  }
+`;
+
+const Cemetery = () => {
+  const [banks] = useBanks();
+  const { path } = useRouteMatch();
+  const { account } = useWallet();
+  const activeBanks = banks.filter((bank) => !bank.finished);
+  return (
+    <Switch>
+      <Page>
+        <Route exact path={path}>
+          <BackgroundImage />
+          {!!account ? (
+            <Container maxWidth="lg">
+              <Typography color="textPrimary" align="center" variant="h3" gutterBottom>
+                Legacy Dawn
+              </Typography>
+
+              <Box mt={5}>
+                <div hidden={activeBanks.filter((bank) => bank.sectionInUI === 2).length === 0}>
+                  <Alert
+                    style={{ marginTop: '20px', marginBottom: '20px', backgroundColor: '#b43387', fontSize: '20px' }}
+                    variant="filled"
+                    severity="warning"
+                  >
+                    <b>All below pools have ended. Please unstake and collect your rewards.</b>
+                  </Alert>
+                  <Typography
+                    align="center"
+                    color="textPrimary"
+                    variant="h4"
+                    gutterBottom
+                    style={{ marginTop: '20px' }}
+                  >
+                    Earn SPOLAR by staking LP
+                  </Typography>
+                  <Grid container spacing={3}>
+                    {activeBanks
+                      .filter((bank) => bank.sectionInUI === 3)
+                      .map((bank) => (
+                        <React.Fragment key={bank.name}>
+                          <CemeteryCard bank={bank} />
+                        </React.Fragment>
+                      ))}
+                  </Grid>
+                </div>
+                <div hidden={activeBanks.filter((bank) => bank.sectionInUI === 4).length === 0}>
+                  <Typography
+                    align="center"
+                    color="textPrimary"
+                    variant="h4"
+                    gutterBottom
+                    style={{ marginTop: '20px' }}
+                  >
+                    Lunar Genesis Pools
+                  </Typography>
+
+                  <Grid container spacing={3}>
+                    {activeBanks
+                      .filter((bank) => bank.sectionInUI === 4)
+                      .map((bank) => (
+                        <React.Fragment key={bank.name}>
+                          <CemeteryCard bank={bank} />
+                        </React.Fragment>
+                      ))}
+                  </Grid>
+                </div>
+                <div hidden={activeBanks.filter((bank) => bank.sectionInUI === 1).length === 0}>
+                  <Typography
+                    align="center"
+                    color="textPrimary"
+                    variant="h4"
+                    gutterBottom
+                    style={{ marginTop: '20px' }}
+                  >
+                    Earn POLAR by staking LP
+                  </Typography>
+                  <Grid container spacing={3} style={{ marginTop: '20px' }}>
+                    {activeBanks
+                      .filter((bank) => bank.sectionInUI === 1)
+                      .map((bank) => (
+                        <React.Fragment key={bank.name}>
+                          <CemeteryCard bank={bank} />
+                        </React.Fragment>
+                      ))}
+                  </Grid>
+                </div>
+
+                <div hidden={activeBanks.filter((bank) => bank.sectionInUI === 0).length === 0}>
+                  <Typography
+                    align="center"
+                    color="textPrimary"
+                    variant="h4"
+                    gutterBottom
+                    style={{ marginTop: '20px' }}
+                  >
+                    Polar Genesis Pools
+                  </Typography>
+
+                  <Grid container spacing={3}>
+                    {activeBanks
+                      .filter((bank) => bank.sectionInUI === 0)
+                      .map((bank) => (
+                        <React.Fragment key={bank.name}>
+                          <CemeteryCard bank={bank} />
+                        </React.Fragment>
+                      ))}
+                  </Grid>
+                </div>
+              </Box>
+            </Container>
+          ) : (
+            <UnlockWallet />
+          )}
+        </Route>
+        <Route path={`${path}/:bankId`}>
+          <BackgroundImage />
+          <Bank />
+        </Route>
+      </Page>
+    </Switch>
+  );
+};
+
+export default Cemetery;
---
+++ src/views/LegacyDawn/DawnCard.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Button, Card, CardActions, CardContent, Typography, Grid } from '@material-ui/core';
+
+import TokenSymbol from '../../components/TokenSymbol';
+import CardIcon from '../../components/CardIcon';
+const CemeteryCard = ({ bank }) => {
+  return (
+    <Grid item xs={12} md={12} lg={12}>
+      <Card>
+        <CardContent align="center" style={{ position: 'relative', paddingBottom: '16px' }}>
+          <Grid container alignItems="center">
+            <Grid container item sm={4} alignItems="center">
+              <Box mr={5} ml={5} mt={2}>
+                <CardIcon>
+                  <TokenSymbol symbol={bank.depositTokenName} />
+                </CardIcon>
+              </Box>
+              <Typography variant="h5" component="h2">
+                {bank.depositTokenName}
+              </Typography>
+            </Grid>
+
+            <Grid item sm={4}>
+              <Typography color="textSecondary">
+                {/* {bank.name} */}
+                Deposit {bank.depositTokenName.toUpperCase()} Earn {` ${bank.earnTokenName}`}
+              </Typography>
+            </Grid>
+            <Grid item sm={4}>
+              <Button color="primary" size="small" variant="contained" component={Link} to={`/dawn/${bank.contract}`}>
+                VIEW & STAKE
+              </Button>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+};
+
+export default CemeteryCard;
