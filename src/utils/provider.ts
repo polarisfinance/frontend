e4435621c773da4906417edd9079b40ce57a1c7b
horviskoadrianko <horviskoadrianko@gmail.com>
2022-05-01 12:25:55 +0200

    add reconnect to wss

--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -1,12 +1,25 @@
 import { ethers } from 'ethers';
 import config from '../config';
-import { web3ProviderFrom } from '../tomb-finance/ether-utils';
+import Web3WsProvider from 'web3-providers-ws';
 
 let provider: ethers.providers.Web3Provider = null;
 
 export function getDefaultProvider(): ethers.providers.Web3Provider {
   if (!provider) {
-    provider = new ethers.providers.Web3Provider(web3ProviderFrom(config.defaultWssProvider), config.chainId);
+    provider = new ethers.providers.Web3Provider(
+      new (Web3WsProvider as any)(config.defaultWssProvider, {
+        clientConfig: {
+          keepalive: true,
+          keepaliveInterval: 60000,
+        },
+        reconnect: {
+          auto: true,
+          delay: 5000,
+          maxAttempts: 100,
+          onTimeout: true,
+        },
+      }),
+    );
   }
 
   return provider;
