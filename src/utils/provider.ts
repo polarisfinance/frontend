e00ebfeab6d2aa8cd9fd562c72a1191c89c61bdb
horviskoadrianko <horviskoadrianko@gmail.com>
2022-05-02 18:17:46 +0200

    back to https provider

--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -1,25 +1,12 @@
 import { ethers } from 'ethers';
 import config from '../config';
-import Web3WsProvider from 'web3-providers-ws';
+import { web3ProviderFrom } from '../tomb-finance/ether-utils';
 
 let provider: ethers.providers.Web3Provider = null;
 
 export function getDefaultProvider(): ethers.providers.Web3Provider {
   if (!provider) {
-    provider = new ethers.providers.Web3Provider(
-      new (Web3WsProvider as any)(config.defaultWssProvider, {
-        clientConfig: {
-          keepalive: true,
-          keepaliveInterval: 60000,
-        },
-        reconnect: {
-          auto: true,
-          delay: 5000,
-          maxAttempts: 100,
-          onTimeout: true,
-        },
-      }),
-    );
+    provider = new ethers.providers.Web3Provider(web3ProviderFrom(config.defaultWssProvider), config.chainId);
   }
 
   return provider;
