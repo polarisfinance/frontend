aa56ac1e614b2eb5aac0e3d9dbed9000144b24fc
horviskoadrianko <horviskoadrianko@gmail.com>
2022-05-21 23:24:22 +0200

    add border radius to card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,11 +4,12 @@ import styled from 'styled-components';
 const Card: React.FC = ({ children }) => <StyledCard>{children}</StyledCard>;
 
 const StyledCard = styled.div`
-  background-color: rgba(71,32,123,0.9); //${(props) => props.theme.color.grey[800]};
-  color: #FFFFFF !important;
+  background-color: rgba(71, 32, 123, 0.9); //${(props) => props.theme.color.grey[800]};
+  color: #ffffff !important;
   display: flex;
   flex: 1;
   flex-direction: column;
+  border-radius: 10px !important;
 `;
 
 export default Card;
